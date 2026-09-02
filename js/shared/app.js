eb9cd779579135d6e515922000f25bc6edb2a561
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-02 19:03:13 +0800

    Create shared app bootstrap

---
+++ js/shared/app.js
@@ -0,0 +1,9 @@
+import{setupLogin}from"./login.js";
+import{applyTimeAtmosphere}from"./atmosphere.js";
+import{setupPublicMode}from"../public/mode.js";
+import{setupPrivateMode}from"../private/mode.js";
+const app=document.getElementById("app");
+const publicMode=setupPublicMode();
+setupPrivateMode({app,publicMode});
+setupLogin({app,frontWorld:publicMode.world});
+applyTimeAtmosphere();
