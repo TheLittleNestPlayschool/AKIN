42525f19bc78b70a3b0f2891a2047a5a413b5493
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-02 19:45:32 +0800

    Wire hidden entry onboarding and authentication

--- js/shared/app.js
+++ js/shared/app.js
@@ -1,10 +1,13 @@
-
 import{setupLogin}from"./login.js";
 import{applyTimeAtmosphere}from"./atmosphere.js";
 import{setupPublicMode}from"../public/mode.js";
 import{setupPrivateMode}from"../private/mode.js";
+import{setupPrivateOnboarding}from"../private/onboarding.js";
+import{setupPrivateEntry}from"../private/entry.js";
 const app=document.getElementById("app");
 const publicMode=setupPublicMode();
-setupPrivateMode({app,publicMode});
-setupLogin({app,frontWorld:publicMode.world});
+const privateMode=setupPrivateMode({app,publicMode});
+const privateOnboarding=setupPrivateOnboarding();
+setupPrivateEntry({publicMode,privateMode});
+setupLogin({app,frontWorld:publicMode.world,onEnter:()=>{if(!privateOnboarding.isComplete())privateOnboarding.show()}});
 applyTimeAtmosphere();
