e0562baa03cfe79b6a0b87b0fe3fc3cd60ae8d86
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-03 12:39:39 +0800

    Use backend bootstrap state after authentication

--- js/shared/app.js
+++ js/shared/app.js
@@ -1,14 +1,23 @@
-
 import{setupLogin}from"./login.js";
 import{applyTimeAtmosphere}from"./atmosphere.js";
 import{setupPublicMode}from"../public/mode.js";
 import{setupPrivateMode}from"../private/mode.js";
 import{setupPrivateOnboarding}from"../private/onboarding.js";
 import{setupPrivateEntry}from"../private/entry.js";
+
 const app=document.getElementById("app");
 const publicMode=setupPublicMode();
 const privateMode=setupPrivateMode({app,publicMode});
 const privateOnboarding=setupPrivateOnboarding();
 setupPrivateEntry({publicMode,privateMode});
-setupLogin({app,frontWorld:publicMode.world,onEnter:()=>{if(!privateOnboarding.isComplete())privateOnboarding.show()}});
+
+setupLogin({
+  app,
+  frontWorld:publicMode.world,
+  onEnter:bootstrapData=>{
+    const onboardingComplete=Boolean(bootstrapData?.w_user?.onboarding_complete);
+    if(!onboardingComplete&&!privateOnboarding.isComplete())privateOnboarding.show();
+  }
+});
+
 applyTimeAtmosphere();
